590a94321bb4396490fa70c916c1806b133c5873
ö <daveyeb@gmail.com>
2021-03-18 18:09:06 -0400

    Update rm_parser.h

--- src/parser/rm_parser.h
+++ src/parser/rm_parser.h
@@ -1,11 +1,11 @@
 #ifndef RM_PARSER
 #define RM_PARSER
 
-#include "../rules/rm_spec_java.h"
+#include "../rules/rm_spec_java_refined.h"
 
-static inline int scan_token(lexer *lex, struct language *lang, char **tokens);
+static inline int scan_token(lexer *lex, struct language *lang, rm_array * a);
 
-static inline int scan_token(lexer *lex, struct language *lang, char **tokens)
+static inline int scan_token(lexer *lex, struct language *lang, rm_array * a)
 {
     int tmp_char; // needed to skip whitespaces and illegal chars
 
@@ -13,14 +13,14 @@ static inline int scan_token(lexer *lex, struct language *lang, char **tokens)
     {
         int start = lex->current;
 
-        lang->rules.scan_numeric(lex, tokens);
-        lang->rules.scan_identifier(lex, tokens);
-        lang->rules.skip_comment(lex, tokens);
-        lang->rules.scan_punctuation(lex, tokens);
-        lang->rules.scan_literal(lex, tokens);
+        lang->rules.scan_numeric(lex, a);
+
+        // lang->rules.scan_identifier(lex, tokens);
+        // lang->rules.skip_comment(lex, tokens);
+        // lang->rules.scan_punctuation(lex, tokens);
+        // lang->rules.scan_literal(lex, tokens);
 
         _next_char(lex, &tmp_char);
-        // printf("tmp char %c <--\n", tmp_char);
     }
 
     return 0;
