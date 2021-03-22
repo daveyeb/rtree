7fd0d5376a22aa838b1d08822081f8b24c00f422
David Yeböah <thesun@Davids-MBP.attlocal.net>
2021-03-22 01:12:33 -0400

    9

---
+++ src/rm_scanner.h
@@ -0,0 +1,27 @@
+#ifndef RM_SCANNER
+#define RM_SCANNER
+
+#include "rm_spec.h"
+
+static inline int scan_token(lexer *lexer, rm_array *tokens, struct language *lang);
+
+static inline int scan_token(lexer *lexer, rm_array *tokens, struct language *lang)
+{
+    int skip; // needed to skip whitespaces and already consumed chars
+
+    while (lexer->current < lexer->length)
+    {
+
+        lang->rules.scan_numeric(lexer, tokens);
+        lang->rules.scan_identifier(lexer, tokens);
+        lang->rules.skip_comment(lexer, tokens);
+        lang->rules.scan_punctuation(lexer, tokens);
+        lang->rules.scan_literal(lexer, tokens);
+
+        _next_char(lexer, &skip);
+    }
+
+    return 0;
+}
+
+#endif // RM_SCANNER
