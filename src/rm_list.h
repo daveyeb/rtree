6c5ad38643fb1878352332127e31ae749e353993
ö <daveyeb@gmail.com>
2021-03-30 11:53:40 -0400

    Update and rename rm_array.h to rm_list.h

--- src/rm_list.h
+++ src/rm_list.h
@@ -22,51 +22,103 @@ typedef struct list
 
 int rm_list_init(rm_list *array, const size_t data_size);
 int rm_list_init_fill(rm_list *array, const size_t data_size, const size_t size, const void *c);
-int rm_list_copy(rm_list *src, rm_list *dest, const size_t size);
+int rm_list_copy(rm_list *src, rm_list *dest);
 int rm_list_add(rm_list *array, const void *value);
 int rm_list_insert(rm_list *array, const size_t index, const void *value);
 int rm_list_delete(rm_list *array, const size_t index);
-int rm_list_get(rm_list *array, const size_t index, void *value); // get value
+int rm_list_get(rm_list *array, const size_t index, void *value); 
 int rm_list_clear(rm_list *array);
 int rm_list_destroy(rm_list *array);
 
-int _ensure_cap(rm_list * array, const size_t min_cap);
-int _grow(rm_list * array, const size_t min_cap);
-int _huge_cap(const size_t min_cap, size_t * new_cap);
+int _ensure_cap(rm_list *array, const size_t min_cap);
+int _grow(rm_list *array, const size_t min_cap);
+int _huge_cap(const size_t min_cap, int *new_cap);
+int _copy_of(rm_list *old, const size_t size);
 
 // TODO : for each macro
 
-int _ensure_cap(rm_list * array, const size_t min_cap){
-    // investigate modcount 
+int _ensure_cap(rm_list *array, const size_t min_cap)
+{
+    // investigate modcount
 
-    if(min_cap - array->capacity > 0)
+    if (min_cap - array->capacity > 0)
         _grow(array, min_cap);
 
     return RM_SUCCESS;
 }
 
-int _grow(rm_list * array, const size_t min_cap){
-    int old_cap = array->capacity;
-    int new_cap = old_cap + ( old_cap >> 1);
-    
-    if(new_cap - min_cap < 0)
+int _grow(rm_list *array, const size_t min_cap)
+{
+    size_t old_cap = array->capacity;
+    int new_cap = old_cap + (old_cap >> 1);
+
+    if (new_cap - min_cap < 0)
         new_cap = min_cap;
-    
-    if(new_cap - RM_MAX_CAP > 0)
+
+    if ((new_cap - RM_MAX_CAP) > 0)
         _huge_cap(min_cap, &new_cap);
 
-
-    // copy arrays 
-    // array.copyof
+    // copy arrays
+    _copy_of(array, new_cap);
 
     return RM_SUCCESS;
 }
 
-int _huge_cap(const size_t min_cap, size_t * new_cap){
-    if(min_cap < 0)
+int _huge_cap(const size_t min_cap, int *new_cap)
+{
+    if (min_cap < 0)
         return RM_FAIL;
-    
+
     *new_cap = (min_cap > RM_MAX_CAP) ? INT_MAX : RM_MAX_CAP;
+
+    return RM_SUCCESS;
+}
+
+int _copy_of(rm_list *old, const size_t size)
+{
+    int index;
+    void **out;
+
+    if (size < 0)
+        return RM_FAIL;
+
+    index = 0;
+
+    // allocating new array
+    out = calloc(size, sizeof(*out));
+
+    while (index < size)
+    {
+        out[index] = calloc(1, old->data_size);
+
+        index++;
+    }
+
+    // copy old data to new
+
+    index = 0;
+    while (index < old->capacity)
+    {
+        memcpy(out[index], old->data[index], old->data_size);
+
+        index++;
+    }
+
+    // freeing old data
+
+    index = 0;
+    while (index < old->capacity)
+    {
+        free(old->data[index]);
+
+        index++;
+    }
+    free(old->data);
+
+    // replace old data with new
+
+    old->data = out;
+    old->capacity = size;
 
     return RM_SUCCESS;
 }
@@ -107,9 +159,7 @@ int rm_list_init_fill(rm_list *array, const size_t data_size, const size_t size,
 
     index = 0;
 
-    // if(size >= array->capacity){
-
-    // }
+    _ensure_cap(array, size);
 
     while (index < size)
     {
@@ -122,7 +172,7 @@ int rm_list_init_fill(rm_list *array, const size_t data_size, const size_t size,
     return RM_SUCCESS;
 }
 
-int rm_list_copy(rm_list *dest, rm_list *src, const size_t n_size)
+int rm_list_copy(rm_list *dest, rm_list *src)
 {
     int index;
 
@@ -170,18 +220,16 @@ int rm_list_add(rm_list *array, const void *value)
     if (array->size + 1 >= array->capacity)
         return RM_FAIL;
 
+    _ensure_cap(array, ( array->size + 1));
     memcpy((array->data)[array->size], value, array->data_size);
 
     array->size++;
-
-    // TODO: implement a resize functionality.
 
     return RM_SUCCESS;
 }
 
 int rm_list_insert(rm_list *array, const size_t index, const void *value)
 {
-
     if (array == NULL)
         return RM_FAIL;
 
@@ -231,7 +279,6 @@ int rm_list_delete(rm_list *array, const size_t index)
     rm_list_destroy(&tmp);
 
     return RM_FAIL;
-
 }
 
 int rm_list_get(rm_list *array, const size_t index, void *value)
